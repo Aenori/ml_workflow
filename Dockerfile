266e503e895defe510d657a2a54ea19623f9e181
Nicolas <Nicolas Rousset>
2021-01-25 13:17:03 +0100

    Correction example/MLWF_example.ipynb example/MLWF_example_with_output.ipynb

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,18 @@
 from python:3.7 
 
 RUN mkdir /app
-RUN apt-get update && apt-get install -y graphviz vim
+RUN apt-get update && apt-get install -y vim graphviz
 
 WORKDIR /app
 
 RUN python -m pip install --upgrade pip
+
 ADD requirements.txt requirements.test.txt /app/
+
+RUN pip install pydot
 RUN pip install -r requirements.txt
 RUN pip install -r requirements.test.txt
 
 ADD . /app
 
 ENV IS_DOCKER=1
-
-ADD jupyter_notebook_config.json /root/.jupyter
-
-CMD jupyter notebook --ip=0.0.0.0 --port=8080 --allow-root 
-
--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -18,13 +18,21 @@ def compare_or_generate_ref(filename):
         os.remove(filename)
 
 workflow_node_id_re = re.compile('Workflow_node_[0-9]+(&#[0-9]+)?')
+width_re = re.compile('width="[0-9]+pt"')
+view_box_re = re.compile('viewBox="0.00 0.00 ([\d]{2,3}\.[\d]{2}) ([\d]{2,3}\.[\d]{2})"')
+
+def apply_re(content):
+    content = workflow_node_id_re.sub('Workflow_node_XX', content)
+    content = width_re.sub('width="XXXpt"', content)
+    content = view_box_re.sub('viewBox="X X X X"', content)
 
 def compare_to_ref(filename, ref_filename):
     if filename.endswith('.svg'):
         with open(filename, 'r') as output:
             with open(ref_filename, 'r') as ref:
-                content = workflow_node_id_re.sub('Workflow_node_XX', output.read())
-                ref_content = workflow_node_id_re.sub('Workflow_node_XX', ref.read())
+
+                content = apply_re(output.read())
+                ref_content = apply_re(ref.read())
 
                 assert(content == ref_content)
     else:
